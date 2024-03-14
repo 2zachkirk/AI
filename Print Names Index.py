caadf92918e6dde5c5a06921cbaef2a71de12b6d
Kirk <2zach.kirk@gulllakecs.org>
2024-03-14 11:02:28 -0700

    PrintingNamesIndex - Staple - Pre ChatGPT

---
+++ Print Names Index.py
@@ -0,0 +1,11 @@
+# Programmer: Zach Kirk
+# Date: 3.14.24
+# Program: Printing Name Index
+# Resource: https://youtu.be/jRAAaDll34Q?si=SZq8WSYzjrmuAoIA
+
+names = ["Corey","Chris","Travis","Dave"]
+
+index = 0
+for name in names:
+    print(index, name)
+    index += 1 
